11a990c01726e8e8ccedef56961958a5b4e8af2c
diego <diego.porto7@gmail.com>
2025-02-13 12:12:46 +0100

    Renamed file

--- gpu_compute_all_videos.sh
+++ gpu_compute_all_videos.sh
@@ -6,5 +6,7 @@ VIDEO_DIR="./videos/KoNViD_1k_videos"
 # Loop through all video files in the directory
 for video in "$VIDEO_DIR"/*; do
     # Call the main_profiler.py program with the video file as an argument
-    /usr/bin/python3 ./main_profiler.py "$video"
+    # /usr/bin/python3 ./main_profiler_optimized.py "$video"
+    /usr/bin/python3 ./main_profiler_optimized_async.py "$video"
+    # /usr/bin/python3 ./main_profiler.py "$video"
 done
